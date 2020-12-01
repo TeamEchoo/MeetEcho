f893947e0207aede81b19a964a3f195295a93558
Laura <laura.rodcarrrion@gmail.com>
2020-12-01 13:04:52 +0100

    nav bar

--- resources/views/CreateEvents.blade.php
+++ resources/views/CreateEvents.blade.php
@@ -5,6 +5,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="{{ asset('resources/sass/_variable.scss') }}" rel="stylesheet">
         <title>Document</title>
     </head>
     <body>
