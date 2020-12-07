a3c04c5d0b79814cffaef2a591951c1febe96e1f
vanessa <vanessapcbarros@gmail.com>
2020-12-07 17:15:19 +0100

    test: add non admin user can not delete event

--- tests/Feature/DeleteEventTest.php
+++ tests/Feature/DeleteEventTest.php
@@ -34,11 +34,24 @@ class DeleteEventTest extends TestCase
             ->delete(route('events', $event->id))
             ->assertStatus(200);
             $this->assertDatabaseCount('events', 0);
-        // create a event
-        //create a user
-        // act as user/admin
-        // deleteEvent
-        //assertDBcounts 0
+        
+    }
 
+    public function test_non_adminUser_cant_delete_a_specific_event()
+    {
+        $event = Event::factory()->create();
+        $userAdmin = User::create([
+            'name' => 'vanessa',
+            'email' => 'van@ff.org',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'is_admin' => false,
+            'remember_token' => Str::random(10)
+        ]);
+
+        $response = $this->actingAs($userAdmin)
+            ->delete(route('events', $event->id))
+            ->assertStatus(302);
+            $this->assertDatabaseCount('events', 1);
+        
     }
 }
