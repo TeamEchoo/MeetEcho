0010e84a227f54d1491775a433279a8c23d9b6db
vanessa <vanessapcbarros@gmail.com>
2020-12-13 15:43:43 +0100

    style: change style on admin event detail page

--- public/js/app.js
+++ public/js/app.js
@@ -515,8 +515,8 @@ eval("// removed by extract-text-webpack-plugin//# sourceURL=[module]\n//# sourc
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\ismae\Documents\GitHub\MeetEcho\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\ismae\Documents\GitHub\MeetEcho\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/vanessa/web-projects/factoriaF5/echoTeam/MeetEcho/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/vanessa/web-projects/factoriaF5/echoTeam/MeetEcho/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
