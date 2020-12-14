e7863d50f5a381f14ea75b095d4a2654a96a05e8
Lorena <criado.manzaneque@gmail.com>
2020-12-14 12:53:49 +0100

    storeTest are passing

--- tests/Feature/StoreEventTest.php
+++ tests/Feature/StoreEventTest.php
@@ -4,10 +4,14 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
 use Tests\TestCase;
+use App\Models\Event;
+use App\Models\User;
 
 class StoreEventTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
@@ -15,8 +19,69 @@ class StoreEventTest extends TestCase
      */
     public function test_store()
     {
-        $response = $this->get('/CreateEvents');
 
-        $response->assertStatus(200);
+        $userAdmin = User::create([
+            'name' => 'lorena',
+            'email' => 'criado@gmail.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'is_admin' => true,
+        ]);
+
+        $this->actingAs($userAdmin);
+        $response = $this->get('/CreateEvents')
+        ->assertStatus(200)
+        ->assertViewIs('CreateEvents');
+    }
+
+    public function test_adminUser_can_create_and_store_event()
+    {
+        $data = [
+                'title'         => "prueba",
+                'description'   => "hola",
+                'date'          => "2121-12-11",
+                'type'          => "masterclass",
+                'category'      => "workshop",
+                'capacity'      => 12,
+                'instructor'    => "lorena",
+                'link'          => "link"
+        ];
+
+        $userAdmin = User::create([
+            'name' => 'lorena',
+            'email' => 'criado@gmail.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'is_admin' => true,
+        ]);
+
+        $response = $this->actingAs($userAdmin)
+        ->post('/CreateEvents', $data)
+        ->assertStatus(302);
+        $this->assertDatabaseCount('events', 1);
+    }
+
+    public function test_User_cant_create_and_store_event()
+    {
+   
+        $data = [
+                'title'         => "prueba",
+                'description'   => "hola",
+                'date'          => "2121-12-11",
+                'type'          => "masterclass",
+                'category'      => "workshop",
+                'capacity'      => 12,
+                'instructor'    => "lorena",
+                'link'          => "link"
+        ];
+
+        $user = User::create([
+            'name' => 'lorena',
+            'email' => 'criado@gmail.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'is_admin' => false,
+        ]);
+
+        $response = $this->actingAs($user)
+        ->post('/CreateEvents', $data);
+        $this->assertDatabaseCount('events', 0);
     }
 }
